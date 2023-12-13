c7f3e90dde618d517d5b7af21f0e5cbe0f0fd894
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-12-13 14:20:46 +0900

    Add files via upload

---
+++ back.py
@@ -0,0 +1,54 @@
+import os
+from flask import Flask, request, jsonify, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.secret_key = "LectureItna"
+
+CORS(app)
+#chat_session = {}
+chat_session = []
+question_history = []
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        data = request.json
+        name = data.get('id')
+        if name in chat_session:
+            return jsonify({'success': False})
+        else:
+            #session['name'] = name  # Corrected line
+            chat_session.append(name)
+            print(f"Your nickname is {name}")
+            for _ in chat_session:
+                print(f"{_} in session.")
+            return jsonify({'success': True})
+
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    if request.method == 'GET':
+        return jsonify({"question_history": chat_session})
+
+    if request.method == 'POST':
+        data = request.json
+        question = data.get("question")
+        question_history.append(question)
+
+        print(question, " 수신함")
+
+        answer = "ㅎㅇ ㅋㅋ"
+
+        response_data = {"question": question, "answer": answer}
+        return jsonify(response_data)
+    
+@app.route('/logout')
+def logout():
+    session.clear()
+
+if __name__ == '__main__':
+    # app.run(debug=False, host='0.0.0.0')    # 서버 연결할 때 사용
+    print("hi back_end server started")
+    app.run(debug = False, port = 5000)  #로컬에서 실험할 때 사용
