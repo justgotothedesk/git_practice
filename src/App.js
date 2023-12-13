76eab1178ff7bcfb633bdff6ce3251e4ab4211bd
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-12-13 14:58:33 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
     }
 
     const make_session = () => {
-        fetch("https://34.22.100.167:5000/login", {
+        fetch("http://34.22.100.167:5000/login", {
             method: "POST",
             body: JSON.stringify({ id : inputId }),
             headers: {
